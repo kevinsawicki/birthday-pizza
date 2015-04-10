3cea2f180d20e937cb1c2528e4370a0a26192234
Kevin Sawicki <kevinsawicki@gmail.com>
2015-04-09 19:44:17 -0700

    Add missing semicolons

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ var serve = fork(servePath, [], {stdio: 'inherit'});
 serve.on('exit', function(code, signal) {
   console.log('serve terminated', code, signal);
   process.exit(code);
-})
+});
 
 var compilePath = path.join(__dirname, 'node_modules', 'typescript', 'bin', 'tsc');
 var compile = fork(compilePath, ['-w', 'index.ts'], {stdio: 'inherit'});
 compile.on('exit', function(code, signal) {
   console.log('compile terminated', code, signal);
   process.exit(code);
-})
+});
