a8317fbaf7dd2525b46881260930ee86597af4ed
Kevin Sawicki <kevinsawicki@gmail.com>
2015-08-17 07:39:43 -0700

    :art: Let TypeScript handle the semicolons

--- index.ts
+++ index.ts
@@ -1,179 +1,179 @@
 class Orbiter {
-  name: string;
-  period: number;
+  name: string
+  period: number
 
   constructor(name: string, period: number) {
-    this.name = name;
-    this.period = period * 24 * 60 * 60 * 1000;
+    this.name = name
+    this.period = period * 24 * 60 * 60 * 1000
   }
 
   getAge(date: Date): number {
-    var time = Date.now() - date.getTime();
-    return Math.floor(time / this.period);
+    var time = Date.now() - date.getTime()
+    return Math.floor(time / this.period)
   }
 
   getNextBirthday(date: Date): Birthday {
-    var age = this.getAge(date) + 1;
-    var birthday = this.period * age;
-    return new Birthday(this, new Date(date.getTime() + birthday), age);
+    var age = this.getAge(date) + 1
+    var birthday = this.period * age
+    return new Birthday(this, new Date(date.getTime() + birthday), age)
   }
 }
 
 class Birthday {
-  location: Orbiter;
-  date: Date;
-  age: number;
+  location: Orbiter
+  date: Date
+  age: number
 
   constructor(location: Orbiter, date: Date, age: number) {
-    this.location = location;
-    this.date = date;
-    this.age = age;
+    this.location = location
+    this.date = date
+    this.age = age
   }
 
   isToday(): boolean {
-    var today = new Date();
+    var today = new Date()
     return today.getFullYear() === this.date.getFullYear() &&
       today.getDate() === this.date.getDate() &&
-      today.getMonth() === this.date.getMonth();
+      today.getMonth() === this.date.getMonth()
   }
 
   isTomorrow(): boolean {
-    var tomorrow = new Date();
-    tomorrow.setDate(tomorrow.getDate() + 1);
+    var tomorrow = new Date()
+    tomorrow.setDate(tomorrow.getDate() + 1)
 
     return tomorrow.getFullYear() === this.date.getFullYear() &&
       tomorrow.getDate() === this.date.getDate() &&
-      tomorrow.getMonth() === this.date.getMonth();
+      tomorrow.getMonth() === this.date.getMonth()
   }
 
   toAgeString(): string {
     return this.age.toString().split('').reverse().map(function(number, index) {
       if (index > 0 && index % 3 === 0)
-        return number + ',';
+        return number + ','
       else
-        return number;
-    }).reverse().join('');
+        return number
+    }).reverse().join('')
   }
 
   toDateString(): string {
-    var month;
+    var month
     switch (this.date.getMonth()) {
       case 0:
-        month = 'jan';
-        break;
+        month = 'jan'
+        break
       case 1:
-        month = 'feb';
-        break;
+        month = 'feb'
+        break
       case 2:
-        month = 'mar';
-        break;
+        month = 'mar'
+        break
       case 3:
-        month = 'apr';
-        break;
+        month = 'apr'
+        break
       case 4:
-        month = 'may';
-        break;
+        month = 'may'
+        break
       case 5:
-        month = 'june';
-        break;
+        month = 'june'
+        break
       case 6:
-        month = 'july';
-        break;
+        month = 'july'
+        break
       case 7:
-        month = 'aug';
-        break;
+        month = 'aug'
+        break
       case 8:
-        month = 'sept';
-        break;
+        month = 'sept'
+        break
       case 9:
-        month = 'oct';
-        break;
+        month = 'oct'
+        break
       case 10:
-        month = 'nov';
-        break;
+        month = 'nov'
+        break
       case 11:
-        month = 'dec';
-        break;
+        month = 'dec'
+        break
     }
 
-    return month + ' ' + this.date.getDate() + ', ' + this.date.getFullYear();
+    return month + ' ' + this.date.getDate() + ', ' + this.date.getFullYear()
   }
 }
 
 class Planet extends Orbiter {
-  moons: Moon[];
+  moons: Moon[]
 
   constructor(name: string, period: number) {
-    super(name, period);
+    super(name, period)
 
-    this.moons = [];
+    this.moons = []
   }
 
   addMoon(name: string, period: number): Planet {
-    this.moons.push(new Moon(name, period, this));
-    return this;
+    this.moons.push(new Moon(name, period, this))
+    return this
   }
 }
 
 class Moon extends Orbiter {
-  planet: Planet;
+  planet: Planet
 
   constructor(name: string, period: number, planet: Planet) {
-    super(name, period);
-    this.planet = planet;
+    super(name, period)
+    this.planet = planet
   }
 }
 
 class Earth extends Planet {
   constructor() {
-    super('Earth', 365.256363004);
-    this.addMoon('Luna', 27.321582);
+    super('Earth', 365.256363004)
+    this.addMoon('Luna', 27.321582)
   }
 
   getAge(date: Date): number {
     var now = new Date()
-    var birthday = new Date();
-    birthday.setTime(date.getTime());
-    birthday.setFullYear(now.getFullYear());
+    var birthday = new Date()
+    birthday.setTime(date.getTime())
+    birthday.setFullYear(now.getFullYear())
 
-    var age = now.getFullYear() - date.getFullYear();
+    var age = now.getFullYear() - date.getFullYear()
     if (birthday.getTime() > now.getTime())
-      age--;
-    return age;
+      age--
+    return age
   }
 
   getNextBirthday(date: Date): Birthday {
-    var age = this.getAge(date);
+    var age = this.getAge(date)
 
-    var birthday = new Date();
-    birthday.setTime(date.getTime());
-    birthday.setFullYear(date.getFullYear() + age);
+    var birthday = new Date()
+    birthday.setTime(date.getTime())
+    birthday.setFullYear(date.getFullYear() + age)
 
-    var now = new Date();
+    var now = new Date()
     if (now.getFullYear() !== birthday.getFullYear() ||
       now.getDate() !== birthday.getDate() ||
       now.getMonth() !== birthday.getMonth()) {
       birthday.setFullYear(birthday.getFullYear() + 1)
-      age++;
+      age++
     }
 
-    return new Birthday(this, birthday, age);
+    return new Birthday(this, birthday, age)
   }
 }
 
-var planets = [];
+var planets = []
 
-planets.push(new Planet('Mercury', 87.9691));
-planets.push(new Planet('Venus',   224.701));
-planets.push(new Earth());
+planets.push(new Planet('Mercury', 87.9691))
+planets.push(new Planet('Venus',   224.701))
+planets.push(new Earth())
 planets.push(new Planet('Mars',    686.971)
   .addMoon('Phobos',    0.31891023)
-  .addMoon('Deimos',    1.263));
+  .addMoon('Deimos',    1.263))
 planets.push(new Planet('Jupiter', 4332.59)
   .addMoon('Io',        1.7691)
   .addMoon('Europa',    3.5512)
   .addMoon('Ganymede',  7.1546)
-  .addMoon('Callisto',  16.689));
+  .addMoon('Callisto',  16.689))
 planets.push(new Planet('Saturn',  10759.22)
   .addMoon('Mimas',     0.942)
   .addMoon('Enceladus', 1.370218)
@@ -181,188 +181,188 @@ planets.push(new Planet('Saturn',  10759.22)
   .addMoon('Dione',     2.736915)
   .addMoon('Rhea',      4.518212)
   .addMoon('Titan',     15.945)
-  .addMoon('Iapetus',   79.3215));
+  .addMoon('Iapetus',   79.3215))
 planets.push(new Planet('Uranus',  30687.15)
   .addMoon('Miranda',   1.413479)
   .addMoon('Ariel',     2.52)
   .addMoon('Umbriel',   4.144)
   .addMoon('Titania',   8.706234)
-  .addMoon('Oberon',    13.463234));
+  .addMoon('Oberon',    13.463234))
 planets.push(new Planet('Neptune', 60190.03)
-  .addMoon('Triton',    5.876854));
+  .addMoon('Triton',    5.876854))
 planets.push(new Planet('Pluto', 90465)
-  .addMoon('Charon',    6.3872304));
+  .addMoon('Charon',    6.3872304))
 
-var $: any;
+var $: any
 
 function setInitialDate(): void {
-  var initialDate = new Date();
-  initialDate.setFullYear(initialDate.getFullYear() - 18);
+  var initialDate = new Date()
+  initialDate.setFullYear(initialDate.getFullYear() - 18)
 
   if (window.location.hash.length > 1) {
-    var hash = window.location.hash.substring(1).replace(/-/g, ' ');
-    var hashDate = new Date(hash);
+    var hash = window.location.hash.substring(1).replace(/-/g, ' ')
+    var hashDate = new Date(hash)
     if (!isNaN(hashDate.getTime()))
-      initialDate = hashDate;
+      initialDate = hashDate
   }
 
-  $('.js-select-day').val(initialDate.getDate());
-  $('.js-select-month').val(initialDate.getMonth());
-  $('.js-select-year').val(initialDate.getFullYear());
+  $('.js-select-day').val(initialDate.getDate())
+  $('.js-select-month').val(initialDate.getMonth())
+  $('.js-select-year').val(initialDate.getFullYear())
 }
 
 function getSelectedDate(): Date {
-  var day   = parseInt($('.js-select-day').val());
-  var month = parseInt($('.js-select-month').val());
-  var year  = parseInt($('.js-select-year').val());
+  var day   = parseInt($('.js-select-day').val())
+  var month = parseInt($('.js-select-month').val())
+  var year  = parseInt($('.js-select-year').val())
 
-  var date = new Date();
-  date.setFullYear(year, month, day);
-  date.setHours(0);
-  date.setMinutes(0);
-  date.setSeconds(0);
-  date.setMilliseconds(0);
+  var date = new Date()
+  date.setFullYear(year, month, day)
+  date.setHours(0)
+  date.setMinutes(0)
+  date.setSeconds(0)
+  date.setMilliseconds(0)
 
-  return date;
+  return date
 }
 
 function getUpcomingBirthdays(date: Date): Birthday[] {
-  var birthdays = [];
+  var birthdays = []
 
   var addBirthday = function(orbiter: Orbiter) {
-    birthdays.push(orbiter.getNextBirthday(date));
+    birthdays.push(orbiter.getNextBirthday(date))
   }
 
   planets.forEach(function(planet) {
-    addBirthday(planet);
-    planet.moons.forEach(addBirthday);
-  });
+    addBirthday(planet)
+    planet.moons.forEach(addBirthday)
+  })
 
   birthdays.sort(function(birthday1, birthday2) {
-    return birthday1.date.getTime() - birthday2.date.getTime();
-  });
+    return birthday1.date.getTime() - birthday2.date.getTime()
+  })
 
-  return birthdays;
+  return birthdays
 }
 
 function updateLocationHash(): void {
-  var date = getSelectedDate();
+  var date = getSelectedDate()
 
-  var month;
+  var month
   switch (date.getMonth()) {
     case 0:
-      month = 'january';
-      break;
+      month = 'january'
+      break
     case 1:
-      month = 'february';
-      break;
+      month = 'february'
+      break
     case 2:
-      month = 'march';
-      break;
+      month = 'march'
+      break
     case 3:
-      month = 'april';
-      break;
+      month = 'april'
+      break
     case 4:
-      month = 'may';
-      break;
+      month = 'may'
+      break
     case 5:
-      month = 'june';
-      break;
+      month = 'june'
+      break
     case 6:
-      month = 'july';
-      break;
+      month = 'july'
+      break
     case 7:
-      month = 'august';
-      break;
+      month = 'august'
+      break
     case 8:
-      month = 'september';
-      break;
+      month = 'september'
+      break
     case 9:
-      month = 'october';
-      break;
+      month = 'october'
+      break
     case 10:
-      month = 'november';
-      break;
+      month = 'november'
+      break
     case 11:
-      month = 'december';
-      break;
+      month = 'december'
+      break
   }
 
-  var day = date.getDate();
-  var year = date.getFullYear();
-  var hash = month + '-' + day + '-' + year;
-  window.location.hash = hash;
+  var day = date.getDate()
+  var year = date.getFullYear()
+  var hash = month + '-' + day + '-' + year
+  window.location.hash = hash
 }
 
 function updateBirthdays(): void {
-  var date = getSelectedDate();
+  var date = getSelectedDate()
   getUpcomingBirthdays(date).forEach(function(birthday) {
-    var name = birthday.location.name.toLowerCase();
+    var name = birthday.location.name.toLowerCase()
 
-    $('.age-' + name).text(birthday.toAgeString());
-    $('.age-' + name).removeClass('birthday-today birthday-tomorrow');
-    $('.birthday-' + name).removeClass('birthday-today birthday-tomorrow');
+    $('.age-' + name).text(birthday.toAgeString())
+    $('.age-' + name).removeClass('birthday-today birthday-tomorrow')
+    $('.birthday-' + name).removeClass('birthday-today birthday-tomorrow')
 
     if (birthday.isToday()) {
-      $('.age-' + name).addClass('birthday-today');
-      $('.birthday-' + name).addClass('birthday-today');
-      $('.birthday-' + name).text('TODAY!');
+      $('.age-' + name).addClass('birthday-today')
+      $('.birthday-' + name).addClass('birthday-today')
+      $('.birthday-' + name).text('TODAY!')
     } else if (birthday.isTomorrow()) {
-      $('.age-' + name).addClass('birthday-tomorrow');
-      $('.birthday-' + name).addClass('birthday-tomorrow');
-      $('.birthday-' + name).text('tomorrow');
+      $('.age-' + name).addClass('birthday-tomorrow')
+      $('.birthday-' + name).addClass('birthday-tomorrow')
+      $('.birthday-' + name).text('tomorrow')
     } else {
-      $('.birthday-' + name).text(birthday.toDateString());
+      $('.birthday-' + name).text(birthday.toDateString())
     }
-  });
+  })
 }
 
 function updateAvailableDays(): void {
-  var day   = parseInt($('.js-select-day').val());
-  var month = parseInt($('.js-select-month').val());
-  var year  = parseInt($('.js-select-year').val());
-  var leap  = year % 4 === 0;
+  var day   = parseInt($('.js-select-day').val())
+  var month = parseInt($('.js-select-month').val())
+  var year  = parseInt($('.js-select-year').val())
+  var leap  = year % 4 === 0
 
-  $('.js-day').prop('disabled', false);
+  $('.js-day').prop('disabled', false)
 
   switch (month) {
     case 1:  // February
       if (leap) {
-        $('.js-day-30, .js-day-31').prop('disabled', true);
-        if (day > 29) $('.js-select-day').val(29);
+        $('.js-day-30, .js-day-31').prop('disabled', true)
+        if (day > 29) $('.js-select-day').val(29)
       } else {
-        $('.js-day-29, .js-day-30, .js-day-31').prop('disabled', true);
-        if (day > 28) $('.js-select-day').val(28);
+        $('.js-day-29, .js-day-30, .js-day-31').prop('disabled', true)
+        if (day > 28) $('.js-select-day').val(28)
       }
-      break;
+      break
     case 3:  // April
     case 5:  // June
     case 8:  // September
     case 10: // November
-      $('.js-day-31').prop('disabled', true);
-      if (day > 30) $('.js-select-day').val(30);
-      break;
+      $('.js-day-31').prop('disabled', true)
+      if (day > 30) $('.js-select-day').val(30)
+      break
   }
 }
 
 function setBirthdayFromHash() {
-  setInitialDate();
-  updateAvailableDays();
-  updateBirthdays();
+  setInitialDate()
+  updateAvailableDays()
+  updateBirthdays()
 }
 
 function handleEvents() {
   // Enable back/forward navigation by listening to hash changes on the window.
-  $(window).on('hashchange', setBirthdayFromHash);
+  $(window).on('hashchange', setBirthdayFromHash)
 
   $('select').on('change', function() {
-    updateAvailableDays();
-    updateLocationHash();
-    updateBirthdays();
-  });
+    updateAvailableDays()
+    updateLocationHash()
+    updateBirthdays()
+  })
 }
 
 $(function() {
-  setBirthdayFromHash();
-  handleEvents();
-});
+  setBirthdayFromHash()
+  handleEvents()
+})
